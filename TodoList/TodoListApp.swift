7444c7edb5ad036468b3ebabce6cc3a73d8554ea
Thiago Lourenço <thiagolourencosaraiva123@gmail.com>
2026-03-10 17:25:30 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Thiago Lourenço on 10/03/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
